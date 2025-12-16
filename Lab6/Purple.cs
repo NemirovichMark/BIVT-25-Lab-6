4c5ea21b09a56bf25900f59834f7145170b4c557
SIR-WIZIX <egorbezbabchenko@gmail.com>
2025-12-16 15:26:38 +0300

    purple 6-10 not debugged

--- Lab6/Purple.cs
+++ Lab6/Purple.cs
@@ -70,14 +70,10 @@ namespace Lab6
 
       int count = 0;
 
-      for (int i = 0; i < n; i++){
-	int local = 0;
-	for (int j = 0; j < m; j++){
-	  if (matrix[i, j] > 0){
-	    local++;
-	  }
+      for (int j = 0; j < m; j++){
+	if (matrix[row, j] > 0){
+	  count++;
 	}
-	count = Math.Max(local, count);
       }
       return count;
 
@@ -91,14 +87,10 @@ namespace Lab6
 
       int count = 0;
 
-      for (int i = 0; i < m; i++){
-	int local = 0;
-	for (int j = 0; j < n; j++){
-	  if (matrix[j, i] > 0){
-	    local++;
-	  }
+      for (int i = 0; i < n; i++){
+	  if (matrix[i, col] > 0){
+	    count++;
 	}
-	count = Math.Max(local, count);
       }
       return count;
 
@@ -113,11 +105,11 @@ namespace Lab6
       int m2 = B.GetLength(1);
 
       int[,] Ans = new int[n1+1,m1];
-      for (int i = 0; i < n1; i++)
+      for (int i = 0; i <= n1; i++)
       {
 	for (int j = 0; j < m1; j++){
 	  if (i > rowIndex+1){
-	    Ans[i+1, j] = A[i, j];
+	    Ans[i, j] = A[i-1, j];
 	  }
 	  else if (i == rowIndex+1) {
 	    Ans[i, j] = B[j, columnIndex];
@@ -301,7 +293,7 @@ namespace Lab6
 
       for (int i = 0; i < matrix.Length; i++){
 	if (matrix[i] < 0){
-	  matrix[i] = negatives[done--];
+	  matrix[i] = negatives[--done];
 	}
       }
     }
@@ -345,8 +337,6 @@ namespace Lab6
 
     }
 
-    public delegate void SortRowsByMax(int[,] matrix);
-
     public void SwapRows(int ind1, int ind2, ref int[,] matrix)
     {
       
@@ -358,6 +348,17 @@ namespace Lab6
       }
     }
 
+    public int GetRowMax(int[,] matrix, int row)
+    {
+      int max = matrix[row, 0];
+      for (int i = 0; i < matrix.GetLength(1); i++){
+	max = Math.Max(max, matrix[row, i]);
+      }
+      return max;
+    }
+
+    public delegate void SortRowsByMax(int[,] matrix);
+    
     public void SortRowsByMaxAscending(int[,] matrix)
     {
       int n = matrix.GetLength(0);
@@ -365,15 +366,9 @@ namespace Lab6
 
       int[] maxes = new int[n];
       for (int i = 0; i < n; i++){
-	int lmax = matrix[i, 0];
-	for (int j = 0; j < m ;j++){
-	  if (matrix[i, j] > lmax){
-	    lmax = matrix[i, j];
-	  }
-	}
-	maxes[i] = lmax;
+	maxes[i] = GetRowMax(matrix, i);
       }
-
+      return;
       int k = 1;
       while (k < n){
 	if (k == 0 || maxes[k] >= maxes[k - 1]) 
@@ -393,15 +388,10 @@ namespace Lab6
 
       int[] maxes = new int[n];
       for (int i = 0; i < n; i++){
-	int lmax = matrix[i, 0];
-	for (int j = 0; j < m ;j++){
-	  if (matrix[i, j] > lmax){
-	    lmax = matrix[i, j];
-	  }
-	}
-	maxes[i] = lmax;
+	maxes[i] = GetRowMax(matrix, i);
       }
 
+      return;
       int k = 1;
       while (k < n){
 	if (k == 0 || maxes[k] < maxes[k - 1]) 
@@ -419,44 +409,200 @@ namespace Lab6
 
       // code here
 
+      sort(matrix);
+
       // end
 
     }
-    public int[] Task7(int[,] matrix/*, /*FindNegatives find*/)
+
+    public delegate int[] FindNegatives(int[,] matrix);
+
+    public int[] FindNegativeCountPerRow(int[,] matrix){
+      int[] ans = new int[matrix.GetLength(0)];
+      for (int i =0; i < matrix.GetLength(0); i++){
+	for (int j = 0; j < matrix.GetLength(1); j++){
+	  if (matrix[i, j] < 0){
+	    ans[i]++;
+	  }
+	}
+      }
+      return ans;
+    }
+
+    public int[] FindMaxNegativePerColumn(int[,] matrix){
+      int[] ans = new int[matrix.GetLength(1)];
+      for (int i = 0; i < matrix.GetLength(1); i++){
+	for (int j = 0; j < matrix.GetLength(0); j++){
+	  ans[i] = Math.Max(matrix[j, i], ans[i]++);
+	}
+      }
+      return ans;
+
+    }
+
+    public int[] Task7(int[,] matrix, FindNegatives find)
     {
       int[] negatives = null;
 
       // code here
 
+      negatives = find(matrix);
+
       // end
 
       return negatives;
     }
-    public int[,] Task8(int[,] matrix/*, MathInfo info*/)
+
+    public delegate int[,] MathInfo(int[,] matrix);
+
+    public int[,] DefineSeq(int[,] matrix){
+      bool up = false;
+      bool down = false;
+
+      for (int i = 1; i < matrix.GetLength(1) && (up == false || down == false); i++){
+	if (matrix[1, i-1] <= matrix[1, i]) up = true;
+	if (matrix[1, i-1] >= matrix[1, i]) down = true;
+      }
+      if (up == down)return new int[,] {{0}};
+      if (up == true)return new int[,] {{1}};
+      return new int[,] {{-1}};
+    }
+
+    public int[,] FindAllSeq(int[,] matrix){
+      int count = 1;
+      int last = 0;
+      for (int i = 1; i < matrix.GetLength(1); i++){
+	if (matrix[1, i] > matrix[1, i-1] && last <= 0) count++;
+	if (matrix[1, i] < matrix[1, i-1] && last >= 0) count++;
+      }
+
+      int[,] ans = new int[count, 2];
+      last = 0;
+      count = 0;
+      for (int i = 1; i < matrix.GetLength(1); i++){
+	if (matrix[1, i] > matrix[1, i-1] && last <= 0){
+	  if (count == 0){
+	    ans[count, 0] = matrix[0, 0];
+	    ans[count, 1] = matrix[0, i-1];
+	  }
+	  else{
+	    ans[count, 0] = ans[count-1, 1];
+	    ans[count, 1] = matrix[0, i-1];
+	  }
+	}
+      }
+      return ans;
+    }
+
+    public int[,] FindLongestSeq(int[,] matrix)
+    {
+      int[,] data = FindAllSeq(matrix);
+      int [,] ans = new int[1,2]{{0, -1}};
+      for (int i = 0; i< data.GetLength(0); i++){
+	if (data[i, 1] - data[i, 0] > ans[0, 1] - ans[0, 0]){
+	  ans[0, 0] = data[i, 0];
+	  ans[0, 1] = data[i, 1];
+	}
+      }
+      return ans;
+    }
+
+    public int[,] Task8(int[,] matrix, MathInfo info)
     {
       int[,] answer = null;
 
       // code here
 
+      answer = info(matrix);     
+
       // end
 
       return answer;
     }
+
+    public int CountSignFlips(double a, double b, double h, Func<double, double> func)
+    {
+      int count = 0;
+      int last_sign = 0;
+      for (double x = a; x < b+0.00001; x+=h){
+	int sign = Math.Sign(func(x));
+	if (sign != last_sign && last_sign != 0){
+	  count++;
+	}
+      }
+      return count;
+    }
+
+    public delegate double Func(double x);
+
+    public double FuncA(double x){
+      return x*x - Math.Sin(x);
+    }
+
+    public double FuncB(double x){
+      return Math.Exp(x) - 1;
+    }
+
     public int Task9(double a, double b, double h, Func<double, double> func)
     {
       int answer = 0;
 
       // code here
+      answer = CountSignFlips(a, b, h, func);
 
       // end
 
       return answer;
     }
+
+    public delegate void Action(int[][] array);
+
+    public void SortInCheckersOrder(int[][] array){
+      for (int i = 0; i < array.Length; i++){
+	if (i%2 == 0){
+	  Array.Sort(array[i]);
+	}
+	else{
+	  Array.Sort(array[i]);
+	  Array.Reverse(array[i]);
+	}
+      }
+    }
+
+    public void SortBySumDesc(int[][] array){
+      int[] sums = new int[array.Length];
+      for (int i = 0; i < array.Length; i++){
+	for (int j = 0; j < array[i].Length; j++){
+	  sums[i]+=array[i][j];
+	}
+      }
+
+      int k = 1;
+      while (k < array.Length){
+	if (k == 0 || sums[k] >= sums[k - 1]){
+	  k++;
+	}
+	else{
+	  (sums[k], sums[k - 1]) = (sums[k - 1], sums[k]);
+	  (array[k], array[k - 1]) = (array[k - 1], array[k]);
+	  k--;
+	}
+      }
+    }
+    
+    public void TotalReverse(int[][] array){
+      for (int i = 0; i < array.Length; i++){
+	Array.Reverse(array[i]);
+      }
+      Array.Reverse(array);
+    }
+
     public void Task10(int[][] array, Action<int[][]> func)
     {
 
       // code here
 
+      func(array);
       // end
 
     }
