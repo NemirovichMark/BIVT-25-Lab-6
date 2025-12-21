9af71fe4b2df3bdb924e6a22be017aa1e860dbd7
KiraFlux <pret2titio1n@gmail.com>
2025-12-21 15:47:16 +0300

    "Итак" - начало положено.

--- Lab6/Purple.cs
+++ Lab6/Purple.cs
@@ -1,25 +1,39 @@
-namespace Lab6
-{
-    public class Purple
-    {
+namespace Lab6 {
+    public class Purple {
 
-        const int error_invalid_index = -1;
 
-        public void Task1(int[,] A, int[,] B)
-        {
+        public void Task1(int[,] A, int[,] B) {
             // code here
             if (!IsSameSize(A, B)) { return; }
 
             if (!IsSquare(A)) { return; }
             var a_max_item_index = FindDiagonalMaxIndex(A);
-            if (error_invalid_index == a_max_item_index) { return; }
 
             if (!IsSquare(B)) { return; }
             var b_max_item_index = FindDiagonalMaxIndex(A);
-            if (error_invalid_index == b_max_item_index) { return; }
 
             SwapRowColumn(A, a_max_item_index, B, b_max_item_index);
             // end
+        }
+
+        public int FindDiagonalMaxIndex<T>(T[,] matrix) where T : IComparable<T> {
+            int max_index = 0;
+
+            for (var i = 1; i < matrix.GetLength(0); i += 1) {
+                if (matrix[i, i].CompareTo(matrix[max_index, max_index]) > 0) {
+                    max_index = i;
+                }
+            }
+
+            return max_index;
+        }
+
+        public void SwapRowColumn(int[,] matrix, int rowIndex, int[,] B, int columnIndex) {
+            var size = matrix.GetLength(0);
+
+            for (var i = 0; i < size; i += 1) {
+                Swap(ref matrix[rowIndex, i], ref matrix[i, columnIndex]);
+            }
         }
 
         static bool IsSquare<T>(T[,] matrix) => matrix.GetLength(0) == matrix.GetLength(1);
@@ -28,78 +42,91 @@ namespace Lab6
 
         static bool IsSameSize<T>(T[,] A, T[,] B) => IsSameSize(A, B, 0) && IsSameSize(A, B, 1);
 
-        static void Swap<T>(ref T a, ref T b)
-        {
+        static void Swap<T>(ref T a, ref T b) {
             (a, b) = (b, a);
         }
 
-        static int FindDiagonalMaxIndex<T>(T[,] matrix) where T : IComparable<T>
-        {
-            int max_index = 0;
+        //
 
-            for (var i = 1; i < matrix.GetLength(0); i += 1)
-            {
-                if (matrix[i, i].CompareTo(matrix[max_index, max_index]) > 0)
-                {
-                    max_index = i;
-                }
-            }
-
-            return max_index;
-        }
-
-        static void SwapRowColumn(int[,] matrix, int rowIndex, int[,] B, int columnIndex)
-        {
-            var size = matrix.GetLength(0);
-
-            for (var i = 0; i < size; i += 1)
-            {
-                Swap(ref matrix[rowIndex, i], ref matrix[i, columnIndex]);
-            }
-        }
-
-        public void Task2(ref int[,] A, int[,] B)
-        {
+        public void Task2(ref int[,] A, int[,] B) {
 
             // code here
 
             // end
 
         }
-        public void Task3(int[,] matrix)
-        {
+
+        public void InsertColumn(ref int[,] A, int rowIndex, int columnIndex, int[,] B) {
+
+        }
+
+        public int CountPositiveElementsInRow(int[,] matrix, int row) { return 0; }
+
+        public int CountPositiveElementsInColumn(int[,] matrix, int col) { return 0; }
+
+        //
+
+        public void Task3(int[,] matrix) {
 
             // code here
 
             // end
 
         }
-        public void Task4(int[,] A, int[,] B)
-        {
+
+
+        public void ChangeMatrixValues(int[,] matrix) { }
+
+        //
+
+        public void Task4(int[,] A, int[,] B) {
 
             // code here
 
             // end
 
         }
-        public void Task5(int[] matrix, Sorting sort)
-        {
+
+        public int[] CountNegativesPerRow(int[,] matrix) { return null; }
+
+        public int FindMaxIndex(int[] array) { return 0; }
+
+        //
+
+        public void Task5(int[] matrix, Sorting sort) {
 
             // code here
 
             // end
 
         }
-        public void Task6(int[,] matrix, SortRowsByMax sort)
-        {
+        public delegate void Sorting(int[] matrix);
+
+        public void SortNegativeAscending(int[] matrix) { }
+
+        public void SortNegativeDescending(int[] matrix) { }
+
+        //
+
+        public void Task6(int[,] matrix, SortRowsByMax sort) {
 
             // code here
 
             // end
 
         }
-        public int[] Task7(int[,] matrix, FindNegatives find)
-        {
+
+        public delegate void SortRowsByMax(int[,] matirx);
+
+        public void SortRowsByMaxAscending(int[,] matrix) { }
+
+        public void SortRowsByMaxDescending(int[,] matrix) { }
+
+        public int GetRowMax(int[,] matrix, int row) { return 0; }
+
+        //
+
+        public int[] Task7(int[,] matrix, FindNegatives find) {
             int[] negatives = null;
 
             // code here
@@ -108,8 +135,16 @@ namespace Lab6
 
             return negatives;
         }
-        public int[,] Task8(int[,] matrix, MathInfo info)
-        {
+
+        public delegate int[] FindNegatives(int[,] matrix);
+
+        public int[] FindNegativeCountPerRow(int[,] matrix) { return []; }
+
+        public int[] FindMaxNegativePerColumn(int[,] matrix) { return []; }
+
+        //
+
+        public int[,] Task8(int[,] matrix, MathInfo info) {
             int[,] answer = null;
 
             // code here
@@ -118,8 +153,18 @@ namespace Lab6
 
             return answer;
         }
-        public int Task9(double a, double b, double h, Func<double, double> func)
-        {
+
+        public delegate int[,] MathInfo(int[,] matrix);
+
+        public int[,] DefineSeq(int[,] matrix) { return null; }
+
+        public int[,] FindAllSeq(int[,] matrix) { return null; }
+
+        public int[,] FindLongestSeq(int[,] matrix) { return null; }
+
+        //
+
+        public int Task9(double a, double b, double h, Func<double, double> func) {
             int answer = 0;
 
             // code here
@@ -128,13 +173,27 @@ namespace Lab6
 
             return answer;
         }
-        public void Task10(int[][] array, Action<int[][]> func)
-        {
+
+        public int CountSignFlips(double a, double b, double h, Func<double, double> func) { return 0; }
+
+        public double FuncA(double x) { return 0; }
+
+        public double FuncB(double x) { return 0; }
+
+        //
+
+        public void Task10(int[][] array, Action<int[][]> func) {
 
             // code here
 
             // end
 
         }
+
+        public void SortInCheckersOrder(int[][] array) { }
+
+        public void SortBySumDesc(int[][] array) { }
+
+        public void TotalReverse(int[][] array) { }
     }
 }
