4786987b4cd5d48d0dc0862c8d863baaa76c543a
Maygdan <alesharet4253@gmail.com>
2025-12-21 23:54:46 +0300

    My changes

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -6,6 +6,34 @@ namespace Lab6
     {
         public static void Main()
         {
+            Console.WriteLine(Task1(10,10,10));
+        }
+
+        
+        static int Task1(int a,int b, int c)
+        {   
+            int s=0;
+            int[] ar={a,b,c};
+            foreach(int i in ar)
+            {
+                s+=CountDivision(i);
+            }
+            return s;
+        }
+        static int CountDivision(int num)
+        {   int f=0;
+            for(int i = 1; i < Math.Floor(Math.Sqrt(num))+1; i++)
+            {
+                if (num % i == 0)
+                {
+                    if (i != num/i)
+                    {
+                        f++;
+                    }    
+                    f++; 
+                } 
+            }
+            return f;
         }
     }
 }
